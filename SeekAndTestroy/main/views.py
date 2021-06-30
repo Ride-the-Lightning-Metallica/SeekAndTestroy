7007b8b24986fa3f15bb360f2f2cf8b8d61beba1
Ride-the-Lightning-Metallica <stepan.krasilnikov.06@mail.ru>
2021-06-30 15:38:37 +0600

    Added pagination

--- SeekAndTestroy/main/views.py
+++ SeekAndTestroy/main/views.py
@@ -13,6 +13,7 @@ class TestListView(ListView):
     model = Test
     template_name = "main/main.html"
     context_object_name = 'tests'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = add_in_context(super().get_context_data(**kwargs))
